908231b5983d278d2c24f032256aefca424f1769
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-06-23 16:19:09 +0300

    React labs

---
+++ labs/src/App.js
@@ -0,0 +1,15 @@
+import InputComponent from './InputComponent/input'
+import SliderComponent from './SliderComponent/slider'
+import './App.css';
+
+function App() {
+  return (
+    <div className='fluid-container'>
+      <InputComponent />
+      <SliderComponent/>
+    </div>
+
+  );
+}
+
+export default App;
